0820630f79df84f16d8b0a4b213185d8f66d59c7
sheebasharon <sheebasharon1211@gmail.com>
2026-02-01 20:18:16 +0530

    changed localhost to 127.0.0.1 in app.js

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ export default function GovMithra() {
     setIsBotTyping(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/chat', {
+      const response = await fetch('http://127.0.0.1:5000/api/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query: query })
