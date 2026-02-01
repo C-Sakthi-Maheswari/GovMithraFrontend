a6fba1331b7fffeebe4b5717cd4948e488a4f3d7
C-Sakthi-Maheswari <sakthi2k414@gmail.com>
2026-02-01 23:36:17 +0530

    70 percent structured

--- backend/tempchatbot.py
+++ backend/tempchatbot.py
@@ -17,7 +17,7 @@ def simple_stem(word):
     return word
 
 # -------------------- LOAD DATA --------------------
-with open("transport_structured.json", "r", encoding="utf-8") as f:
+with open("agriculture_structured.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 if isinstance(data, dict):
     print("⚠️ Detected JSON is a Dictionary. Converting values to List.")
